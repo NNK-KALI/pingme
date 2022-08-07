3af64ba477bb76d76bc247a5ffbb7030492bab1f
NNK-KALI <neerajkumar18971@gmail.com>
2022-08-08 00:45:24 +0530

    First Commit

---
+++ pingme.py
@@ -0,0 +1,26 @@
+import telebot
+from os import environ
+
+API_TOKEN = environ["TELEGRAM_API_TOKEN"]
+ALERT_RECIEVER_ID = environ["TELEGRAM_ALERT_REC_ID"]
+
+
+bot = telebot.TeleBot(API_TOKEN)
+
+# Handle /start 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message( message.chat.id, "If You know who I am then you know me.", )
+
+
+@bot.message_handler(commands=['ping'])
+def ping_me(message):
+    code = message.text
+    code = code.strip("/ping ")
+    bot.send_message(ALERT_RECIEVER_ID, "U have got  an alert from  {}".format(message.chat.id))
+    
+
+
+
+
+bot.infinity_polling()
